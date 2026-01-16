bafe989cdd806595d58d42e0f0144476712b69d3
Ethan_Huang <117412744+EthanAlong@users.noreply.github.com>
2026-01-16 08:53:37 +0000

    Added Analytics from vercel

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 // src/app/page.js
 "use client"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import RingInterface from '@/components/canvas/RingInterface'
 
 export default function Home() {
   return (
     <main style={{ width: '100vw', height: '100vh', background: '#000' }}>
       <SpeedInsights />
+      <Analytics />
       <RingInterface />
     </main>
   )
