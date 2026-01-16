521fc6d2932949c2a5dc5c2a25c9842337145a0d
Ethan_Huang <117412744+EthanAlong@users.noreply.github.com>
2026-01-16 08:50:28 +0000

    Test for Vercel speedinsights

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 // src/app/page.js
 "use client"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import RingInterface from '@/components/canvas/RingInterface'
 
 export default function Home() {
   return (
     <main style={{ width: '100vw', height: '100vh', background: '#000' }}>
+      <SpeedInsights />
       <RingInterface />
     </main>
   )
